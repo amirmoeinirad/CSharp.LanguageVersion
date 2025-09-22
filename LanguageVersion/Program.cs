24ca8a75a3b7ca5138776c4494594ad971e1008a
Amir Moeini Rad <moeini@shabakehafzar.com>
2025-09-22 13:46:24 +0330

    Add project files.

---
+++ LanguageVersion/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// Amir Moeini Rad
+// September 2025
+
+// Main Concept: Display the C# Language Version used in the current project.
+
+namespace LanguageVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# Compiler and Language Versions...");
+
+            // Uncomment this line: #error version. Then in the 'Error List' window, you will see something like this:
+            // Compiler version: '4.14.0-3.25412.6 (c167adef)'. Language version: 12.0.
+            // The version numbers will be different based on your .NET version.
+            // #error version
+        }
+    }
+}
